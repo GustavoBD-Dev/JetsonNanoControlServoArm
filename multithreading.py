0e502511454c35fc9945c99e3ef4a374d96c421b
Gustavo Blas Duran <gustavoblasduran1999@gmail.com>
2022-05-09 23:23:59 -0500

    threads in array

--- multithreading.py
+++ multithreading.py
@@ -1,13 +1,59 @@
 import threading
 import time
 
-def hola_mundo(nombre):
-    print("Hola Mundo " + nombre)
-    time.sleep(5)
+positionServos = [90,90,90,90,90,90]
+timeControl = 0.05
+
+
+def moveServo(servo, actualPosition, newAnglePosition):
+    #print("Hola Mundo " + str(servo) + " " + str(actualPosition) + " " + str(newAnglePosition))
+    if newAnglePosition < actualPosition:
+        for i in range(actualPosition, newAnglePosition - 1, -1):
+            # set pos servo increment, set value i
+            # print(servo, ' ', i)
+            positionServos[servo] = i
+            time.sleep(timeControl) # time in seconds
+    elif newAnglePosition > actualPosition:
+        for i in range(actualPosition, newAnglePosition + 1, 1):
+            # set pos servo decrement, set value i
+            # print(servo, ' ', i)
+            positionServos[servo] = i
+            time.sleep(timeControl) # time in seconds
+    else:
+        print('NO MOVE SERVO')
+    print(positionServos)
+
+
+def getExecuteData():
+    import csv
+    storedPositions = []
+    with open('angleServosScene.csv', 'r') as file:
+        csv_reader = csv.reader(file, delimiter = ',')
+        list_of_rows = list(csv_reader)
+        storedPositions = list_of_rows
+    print(storedPositions)
+    return storedPositions
+
+
+
 
 if __name__ == "__main__":
-    thread = threading.Thread(target=hola_mundo, args=("Codi",))
-    thread.start()
-    thread.join()
 
-    print("Hola mundo desde el hilo principal")
+    # Get data of file csv
+    storedPositions = getExecuteData()
+
+    print(positionServos)
+    threadServos = []
+    # thread aux variable
+    thread = None
+
+    for i in range(6):
+        thread = threading.Thread(
+                        target = moveServo, 
+                        args = (i, 90, 120,))
+        threadServos.append(thread) 
+
+    for i in range(6):
+       threadServos[i].start()
+
+
